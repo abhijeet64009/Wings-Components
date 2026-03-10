9bc83a9048f8f5fe855d4bb1ca582cfd94fa1b13
Abhijeet Kumar <abhijeet64009@gmail.com>
2026-03-10 23:50:06 +0530

    Added sample usage of component in test app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,19 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-    implementation(":wings library")
+    implementation(project(":wings library"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/satya/factory/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.satya.factory
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.satya.factory.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val DEFAULT_MESSAGE = "Progress Button Clicked"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.progressBtn.setOnButtonClick {
+            showDefaultToast()
+        }
+    }
+
+    private fun showDefaultToast() {
+        Toast.makeText(this, DEFAULT_MESSAGE, Toast.LENGTH_SHORT).show()
+    }
+}
